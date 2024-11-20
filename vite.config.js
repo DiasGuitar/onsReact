7de0feeb7d92a1f83f19d5ac679d24d86dacbddf
DiasGuitar <brainwin0@gmail.com>
2024-11-20 11:36:16 +0500

    1234

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,35 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@assets': path.resolve(__dirname, './public/assets'),
+      // Настройка путей для локальных файлов
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@js': path.resolve(__dirname, './src/assets/js'),
+      '@css': path.resolve(__dirname, './src/assets/css'),
+      '@plugins': path.resolve(__dirname, './src/assets/plugins'),
+    },
+  },
+  server: {
+    fs: {
+      // Разрешаем доступ к локальным файлам вне проекта, если потребуется
+      strict: false,
     },
   },
   build: {
     rollupOptions: {
-      input: './public/index.html',
+      external: [
+        '/src/assets/js/jquery.min.js',
+        '/src/assets/js/popper.min.js',
+        '/src/assets/js/bootstrap.min.js',
+        '/src/assets/plugins/fancybox-master/dist/jquery.fancybox.min.js',
+        '/src/assets/plugins/owl-carousel/dist/owl.carousel.min.js',
+        '/src/assets/js/main.js',
+      ],
     },
   },
 });
