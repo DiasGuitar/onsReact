dfa168342a8b7558cdd28feea981ddeda12794fb
DiasGuitar <brainwin0@gmail.com>
2024-11-20 10:53:13 +0500

    minor fixes for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-// import "./assets/plugins/owl-carousel/dist/assets/owl.carousel.min.css";
-// import "./assets/plugins/owl-carousel/dist/assets/owl.theme.default.min.css";
-// import "./assets/css/style.css";
-// import "./assets/css/normalize.css"
-// import "./assets/css/responsive.css"
-// import "./assets/css/bootstrap.min.css"
-// import "./assets/fonts/flaticon/flaticon.css"
-// import "./assets/plugins/fancybox-master/dist/jquery.fancybox.min.css"
-// import "./assets/fonts/fontawesome/css/all.min.css"
+import "./assets/plugins/owl-carousel/dist/assets/owl.carousel.min.css";
+import "./assets/plugins/owl-carousel/dist/assets/owl.theme.default.min.css";
+import "./assets/css/style.css";
+import "./assets/css/normalize.css"
+import "./assets/css/responsive.css"
+import "./assets/css/bootstrap.min.css"
+import "./assets/fonts/flaticon/flaticon.css"
+import "./assets/plugins/fancybox-master/dist/jquery.fancybox.min.css"
+import "./assets/fonts/fontawesome/css/all.min.css"
 
 import '@css/bootstrap.min.css';
 import '@css/normalize.css';
